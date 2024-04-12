a7128806199b6916304ea68264bc1d4b445a2cb7
ELDI <heldigard@hotmail.com>
2024-04-12 14:12:24 -0500

    v0.0.1

---
+++ Drogueria.py
@@ -0,0 +1,8 @@
+class Drogueria:
+
+
+    def __init__(self, nombre, nit, telefono):
+        self.__nombre = nombre
+        self.__nit = nit
+        self.__telefono = telefono
+        self.__inventario = {}
--- Medicamento.py
+++ Medicamento.py
@@ -2,25 +2,54 @@ class Medicamento:
 
     def __init__(
             self,
-            codigo,
+            sku,
             nombre_comercial,
             nombre_generico,
+            precio_neto,
             peso,
             cantidad,
-            sin_impuesto,
             impuesto=0.19
     ):
-        self.__codigo = codigo
+        self.__sku = sku
         self.__nombre_comercial = nombre_comercial
         self.__nombre_generico = nombre_generico
+        self.__precio_neto = precio_neto
         self.__peso = peso
         self.__cantidad = cantidad
-        self.__sin_impuesto = sin_impuesto
         self.__impuesto = impuesto
+
+        self.__calcular_precio_bruto()
+
+    @property
+    def sku(self):
+        return self.__sku
+
+    @property
+    def precio_neto(self):
+        return self.__precio_neto
+
+    @property
+    def cantidad(self):
+        return self.__cantidad
+
+    @precio_neto.setter
+    def precio_neto(self, precio_neto):
+        self.__precio_neto = precio_neto
+        self.__calcular_precio_bruto()
+
+    @cantidad.setter
+    def cantidad(self, cantidad):
+        self.__cantidad = cantidad
 
     def __str__(self):
         return f"\n\
-      Código: {self.__codigo}\t \n\
-      Nombre comercial: {self.__nombre_comercial}\t \n\
-      Nombre genérico: {self.__nombre_generico}\t \n\
-      Peso: {self.__peso}"
+      Código: {self.__sku}\t \n\
+      Nombre Comercial: {self.__nombre_comercial}\t \n\
+      Nombre Genérico: {self.__nombre_generico}\t \n\
+      Peso: {self.__peso}\t \n\
+      Precio Neto: {self.__precio_neto}\t \n\
+      Precio Bruto: {self.__precio_bruto}\t \n\
+      Cantidad: {self.__cantidad}\t \n"""
+
+    def __calcular_precio_bruto(self):
+        self.__precio_bruto = self.__precio_neto + (self.__precio_neto * self.__impuesto)
